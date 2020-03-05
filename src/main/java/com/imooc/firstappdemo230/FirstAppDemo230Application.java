7589ff4e09fe300f0214c97168845ddf67700dfb
unknown <dandan3029@mail.dlut.edu.cn>
2020-03-05 10:22:03 +0800

    add: add the origin code

---
+++ src/main/java/com/imooc/firstappdemo230/FirstAppDemo230Application.java
@@ -0,0 +1,13 @@
+package com.imooc.firstappdemo230;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAppDemo230Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAppDemo230Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/imooc/firstappdemo230/FirstAppDemo230ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imooc.firstappdemo230;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAppDemo230ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
